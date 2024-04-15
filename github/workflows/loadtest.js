3b6b1b9499377970e6b528284ff7fd72fc3ac76e
MarcHedeby <162103197+MarcHedeby@users.noreply.github.com>
2024-04-15 13:41:31 +0200

    Create loadtest.js

---
+++ github/workflows/loadtest.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+    const baseUrl = 'http://localhost:5080/weatherforecast';
+
+    const resGetWeatherForecast = http.get(baseUrl);
+    validateResponse(resGetWeatherForecast, 200);
+
+    const invalidId = 'invalid-id';
+    const resGetWeatherForecastInvalidId = http.get(`${baseUrl}/${invalidId}`);
+    validateResponse(resGetWeatherForecastInvalidId, 404);
+
+
+    sleep(Math.random() * 3);
+}
+
+function validateResponse(response, expectedStatusCode) {
+    if (response.status === expectedStatusCode) {
+        console.log(`Test Passed - Expected status code ${expectedStatusCode}`);
+    } else {
+        console.error(`Test Failed - Expected status code ${expectedStatusCode}, but received ${response.status}`);
+    }
+}
