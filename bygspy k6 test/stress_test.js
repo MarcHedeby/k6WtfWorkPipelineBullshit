acf83836640c4af970e033c8fa0b30b3f38fbdb0
MarcHedeby <marc@hedeby.net>
2024-04-15 13:33:09 +0200

    Add project files.

---
+++ bygspy k6 test/stress_test.js
@@ -0,0 +1,26 @@
+﻿import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 1 }, 
+        { duration: '3m', target: 1 }, 
+        { duration: '1m', target: 0 }    
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<10'], 
+        http_req_failed: ['rate<0.01']     
+    }
+};
+
+export default function () {
+    const baseUrl = 'http://localhost:5080/weatherforecast';
+
+    const res = http.get(baseUrl);
+
+    const checkRes = check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    sleep(Math.random() * 3);
+}
