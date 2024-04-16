7e2dc72beaad7809fba0fe446fd87583c4bec601
MarcHedeby <marc@hedeby.net>
2024-04-16 15:22:18 +0200

    qwew

--- bygspy k6 test/WeatherForecast.cs
+++ bygspy k6 test/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace bygspy_k6_test
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 332421 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 332 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
